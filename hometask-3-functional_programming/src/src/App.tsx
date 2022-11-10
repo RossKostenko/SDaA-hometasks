7daf440124b6d2279b036fb511797798db0777ed
Rostyslav Kostenko <rostyslav_kostenko@epam.com>
2022-11-10 13:16:02 +0200

    hw-3-combined-logic-together

--- hometask-3-functional_programming/src/src/App.tsx
+++ hometask-3-functional_programming/src/src/App.tsx
@@ -2,11 +2,11 @@ import { FC } from 'react';
 import { useState, useEffect } from 'react';
 import { StyledEngineProvider } from '@mui/material/styles';
 
+import { mergeToRowData, filterCurry, searchCurry, sortCurry } from './helpers';
 import { Image, User, Account } from '../types';
 import { Table, Filters, Sort, Search, Row } from './components';
 import { getImages, getUsers, getAccounts } from './mocks/api';
 import rows from './mocks/rows.json';
-
 import styles from './App.module.scss';
 
 // mockedData has to be replaced with parsed Promises’ data
@@ -14,32 +14,38 @@ const mockedData: Row[] = rows.data;
 
 export const App: FC = () => {
   const [data, setData] = useState<Row[]>(undefined);
+  const [filterData, setFilterData] = useState<string[]>();
+  const [searchData, setSearchData] = useState<string>('');
+  const [sortData, setSortData] = useState<string>('');
 
   useEffect(() => {
-    // fetching data from API
-    Promise.all([
-      getImages(),
-      getUsers(),
-      getAccounts(),
-    ]).then(([images, users, accounts]: [Image[], User[], Account[]]) => {
-      console.log(images, users, accounts);
-      // setData(value);
+    Promise.all([getImages(), getUsers(), getAccounts()]).then(
+      ([images, users, accounts]: [Image[], User[], Account[]]) => {
+        setData(mergeToRowData(images, users, accounts));
+      }
     );
-    }
-      
   }, []);
+
+  const getCurrentData = (data: Row[]) =>
+    data
+      ? [
+          filterCurry(filterData),
+          searchCurry(searchData),
+          sortCurry(sortData),
+        ].reduce((acc, fn) => fn(acc), data)
+      : null;
 
   return (
     <StyledEngineProvider injectFirst>
       <div className="App">
         <div className={styles.container}>
           <div className={styles.sortFilterContainer}>
-            <Filters />
-            <Sort />
+            <Filters updateStore={setFilterData} />
+            <Sort updateStore={setSortData} />
           </div>
-          <Search />
+          <Search updateStore={setSearchData} />
         </div>
-        <Table rows={data || mockedData} />
+        <Table rows={getCurrentData(data) || mockedData} />
       </div>
     </StyledEngineProvider>
   );
