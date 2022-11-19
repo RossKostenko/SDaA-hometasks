8940258667848b975851b55ec89f253665ca19ad
Rostyslav Kostenko <rkostenko@slb.com>
2022-11-19 18:25:37 +0200

    added shipment state builder

---
+++ hometask-4-desing-patterns/gui/shipment-state/shipment-state-builders.ts
@@ -0,0 +1,57 @@
+import { ShipmentSate } from './shipment-state'
+
+export class ShipmentSateBuilder {
+	constructor(protected shipmentState = new ShipmentSate()) {
+		this.shipmentState = shipmentState;
+	}
+
+	get from() {
+		return new SenderBuilder(this.shipmentState);
+	}
+
+	get to() {
+		return new DeliveryBuilder(this.shipmentState);
+	}
+
+	weigths(weight: number) {
+		this.shipmentState.shipmentId = Math.round(Math.random() * 1000000000000);
+		this.shipmentState.weight = weight
+		return this
+	}
+
+	build() {
+		return this.shipmentState;
+	}
+}
+
+class SenderBuilder extends ShipmentSateBuilder {
+	constructor(shipmentState) {
+		super(shipmentState);
+	}
+
+	adress(adress: string) {
+		this.shipmentState.senderAdress = adress;
+		return this;
+	}
+
+	zip(zip: string) {
+		this.shipmentState.senderZipCode = zip;
+		return this;
+	}
+}
+
+class DeliveryBuilder extends ShipmentSateBuilder {
+	constructor(shipmentState) {
+		super(shipmentState);
+	}
+
+	adress(streetAddress) {
+		this.shipmentState.deliveryAdress = streetAddress;
+		return this;
+	}
+
+	zip(postcode) {
+		this.shipmentState.deliveryZipCode = postcode;
+		return this;
+	}
+}
---
+++ hometask-4-desing-patterns/gui/shipment-state/shipment-state.ts
@@ -0,0 +1,10 @@
+export class ShipmentSate {
+	public shipmentId: number;
+	public weight: number;
+	public senderAdress: string;
+	public senderZipCode: string;
+	public deliveryAdress: string;
+	public deliveryZipCode: string;
+
+	constructor() { }
+}
